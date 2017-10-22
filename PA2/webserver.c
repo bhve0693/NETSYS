b7c1a5411c66c1f124b120132114a4af3862570b
bhve0693 <bhve0693@colorado.edu>
2017-10-21 21:52:07 -0600

    Added HTTP Conn Response, Parsed GET Request and Valid HTTP or not

--- PA2/webserver.c
+++ PA2/webserver.c
@@ -25,28 +25,34 @@
 #include <sys/time.h>
 #include <signal.h>
 
-#define MAX_CONN 100
-#define MAX_WSBUFF 1000
+#define MAX_CONN         100
+#define MAX_WSBUFF 	     1000
+#define TX_BYTES		 1024
+#define CLIENT_MSGSIZE 	 65536
+#define TRUE             1
+#define FALSE            0
+#define STATUSLENGTH     2000
 
 
 char *DIR_ROOT = NULL;
 char PORT_NUM[8];
 int sock = 0;
 int clients[MAX_CONN];
-
+int COUNT = 0;
+int globalClientNumber = 0;
 
 void signal_handler(int signal)
 {
 	switch (signal)
 	{
 		case SIGINT:
-				close(sock);
-				exit(1);
-				break;
+			close(sock);
+			exit(1);
+			break;
 		case SIGTERM:
-				close(sock);
-				exit(1);
-				break;
+			close(sock);
+			exit(1);
+			break;
 
 	}
 }
@@ -61,6 +67,71 @@ static uint32_t get_sizeof_file (FILE * fileDesc)
     fseek(fileDesc, 0L, SEEK_SET);
 
     return size;
+}
+
+
+char * content_check(char *filetype)
+{
+    char *type;
+
+    if((strcmp(filetype,".htm"))==0 || (strcmp(filetype,".html"))==0)
+        type = "text/html";
+    else if((strcmp(filetype,".jpg"))==0)
+        type = "image/jpeg";
+    else if(strcmp(filetype,".gif")==0)
+        type = "image/gif";
+    else if(strcmp(filetype,".txt")==0)
+        type = "text/plain";
+    else if(strcmp(filetype,".png")==0)
+        type = "image/png";
+    else if(strcmp(filetype,".css")==0)
+        type = "text/css";
+    else if(strcmp(filetype,".js")==0)
+        type = "text/javascript";
+    else 
+        type="application/octet-stream";
+
+    return type;
+
+}
+
+int format_extract(char *filetype)
+{
+
+    FILE *fp;
+    char wsbuff[MAX_WSBUFF];
+    unsigned int formatIndex = 0;
+    int file_supported = 0;
+    char formats[20][100];
+ 
+
+    char *wsconfig = getenv("PWD");
+	if (wsconfig != NULL)
+    	printf("Path to ws.conf: %s \n", wsconfig);
+
+    fp=fopen(wsconfig,"r");
+    unsigned int wsconfig_size = get_sizeof_file (fp);
+
+    while(fgets(wsbuff,wsconfig_size,fp)!=NULL) {//read from the .conf file
+        strcpy(formats[formatIndex],wsbuff);
+        formatIndex++;
+    }
+
+    int k=0;
+
+    
+    for(k=0;k<formatIndex+1;k++) {
+        if(strncmp(formats[k],filetype,3)==0) {//check if the file is supported
+            file_supported = 1;//if supported then set file_supported.
+            break;
+        }
+    }
+
+    fclose(fp);
+
+    return file_supported;
+
+
 }
 
 void wsconf_read()
@@ -139,9 +210,157 @@ void start_server(int port)
 }
 
 
-void conn_response(int conn_count)
+void conn_response(int n)
 {
 	printf("\nEntering Conn Response \n");
+	char msg_from_client[CLIENT_MSGSIZE], *req_string[3], msg_to_client[TX_BYTES], path[CLIENT_MSGSIZE];
+    int rcvd, fd, bytes_read;
+    FILE *fp;
+    char status[STATUSLENGTH];
+    char connection_status[50];
+    globalClientNumber = n;
+    int post_req_check=0;
+
+	printf("\n _________________________________\nglobalClientNumber : %d\n",globalClientNumber);
+	while(1){
+
+	post_req_check = 0;  // make it zero before every post request
+	bzero(status, sizeof(status));
+    bzero(msg_from_client, sizeof(msg_from_client));
+    bzero(path, sizeof(path));
+    bzero(req_string, sizeof(req_string));
+    bzero(msg_to_client, sizeof(msg_to_client));
+    bzero(connection_status, sizeof(connection_status));
+
+    memset( (void*)msg_from_client, (int)'\0', CLIENT_MSGSIZE );
+    rcvd=recv(clients[n], msg_from_client, CLIENT_MSGSIZE, 0);
+
+
+	char filename[50] = "Msgstore";
+    char count_str[50];
+    char line_copy[99999];
+    sprintf(count_str,"%d", COUNT);
+    strcat(filename, count_str);
+    FILE *fp_storeMsg = fopen(filename, "w");
+    if (fp_storeMsg != NULL)
+    {
+        fputs(msg_from_client, fp_storeMsg);
+        fclose(fp_storeMsg);
+    }
+	if (!strstr(msg_from_client,"Connection: Keep-alive"))    // capturing the last string from the received message
+	{
+	    strncpy(connection_status, "Connection: Keep-alive", strlen("Connection: Keep-alive"));
+	}
+	else    /* -- If Keep-alive is not found, close the connection --- */
+	{
+		strncpy(connection_status, "Connection: Close",strlen("Connection: Close"));
+	}
+
+    bzero(status, sizeof(status));
+    if (rcvd<0)    // receive error
+        fprintf(stderr,("recv() error\n"));
+    else if (rcvd==0)    // receive socket closed
+    	rcvd = 0;
+
+
+    else    // message received
+    {
+
+    	if (!strstr(msg_from_client,"Connection: Keep-alive"))    // capturing the last string from the received message
+		{
+	        strncpy(connection_status, "Connection: Keep-alive", strlen("Connection: Keep-alive"));
+		}
+		else    /* -- If Keep-alive is not found, close the connection --- */
+		{
+			strncpy(connection_status, "Connection: Close",strlen("Connection: Close"));
+		}
+
+        printf("\n printing msg_from_client %s\n", msg_from_client);
+        printf("Size of rcvd message %ld \n", sizeof(msg_from_client));
+
+        // Now breaking the incoming strng into three different paths
+        req_string[0] = strtok (msg_from_client, " \t\n");
+        if ((strncmp(req_string[0], "GET\0", 4)==0) || (strncmp(req_string[0], "POST\0", 5)==0))
+        {
+        	if (strncmp(req_string[0], "POST\0", 5)==0)
+        	{
+        		printf("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&Com\n");
+        		post_req_check = TRUE;
+        	}
+
+            req_string[1] = strtok (NULL, " \t");
+            req_string[2] = strtok (NULL, " \t\n");
+            
+
+            char http_version[8];
+            if (strncmp(req_string[2], "HTTP/1.1", 8) == 0)
+                strcpy(http_version, "HTTP/1.1");
+            else
+                strcpy(http_version, "HTTP/1.0");
+
+
+            if ( strncmp( req_string[2], "HTTP/1.0", 8)!=0 && strncmp( req_string[2], "HTTP/1.1", 8)!=0 )
+            {
+
+
+                strncat(status,http_version,strlen(http_version));
+                strncat(status," 400 Bad Request",strlen(" 400 Bad Request"));
+                strncat(status,"\n",strlen("\n"));
+                strncat(status,"Content-Type:",strlen("Content-type:"));
+                strncat(status,"NONE",strlen("NONE"));
+                strncat(status,"\n",strlen("\n"));
+                strncat(status,"Content-Length:",strlen("Content-Length:"));
+                strncat(status,"NONE",strlen("NONE"));
+                strncat(status,"\n",strlen("\n"));
+	            strncat(status,connection_status,strlen(connection_status));
+                strncat(status,"\r\n",strlen("\r\n"));
+                strncat(status,"\r\n",strlen("\r\n"));
+                strncat(status,"<HEAD><TITLE>400 Bad Request Reason</TITLE></HEAD>",strlen("<HEAD><TITLE>400 Bad Request Reason</TITLE></HEAD>"));
+                strncat(status,"<html><BODY>>400 Bad Request Reason: Invalid HTTP-Version :",strlen("<BODY>>400 Bad Request Reason: Invalid HTTP-Version :"));
+                strncat(status,"HTTP",strlen("HTTP"));
+                strncat(status,"</BODY></html>",strlen("</BODY></html>"));
+                strncat(status,"\r\n",strlen("\r\n"));
+                printf("%s\n",status);
+               	write(clients[n], status, strlen(status));
+            }
+            
+
+        }
+
+        else
+            {//file not found
+
+                strncat(status,"HTTP/1.1",strlen("HTTP/1.1"));
+                strncat(status,"\n",strlen("\n"));//strncat(status_line,"\r\n",strlen("\r\n"));
+                strncat(status,"Content-Type:",strlen("Content-type:"));
+                strncat(status,"NONE",strlen("NONE"));
+                strncat(status,"\n",strlen("\n"));
+                strncat(status,"Content-Length:",strlen("Content-Length:"));
+                strncat(status,"NONE",strlen("NONE"));
+                strncat(status,"\r\n",strlen("\r\n"));
+                strncat(status,"\r\n",strlen("\r\n"));
+                strncat(status,"<HEAD><TITLE>501 Not Implemented</TITLE></HEAD>",strlen("<HEAD><TITLE>501 Not Implemented</TITLE></HEAD>"));
+                strncat(status,"<BODY>501 Not Implemented: File format not supported:",strlen("<BODY>501 Not Implemented: File format not supported:"));
+                strncat(status,"HTTP/1.1",strlen("HTTP/1.1"));
+                strncat(status,"</BODY></html>",strlen("</BODY></html>"));
+                strncat(status,"\r\n",strlen("\r\n"));
+                write(clients[n], status, strlen(status));   
+             }
+    }
+
+	if (!strstr(msg_from_client,"Connection: Keep-alive"))    
+	{
+
+	}
+	else  
+	{
+	    shutdown (clients[n], SHUT_RDWR);         // Send and recv disabled
+    	close(clients[n]);
+    	clients[n]=-1;
+    	exit(0);
+	}
+}
+
 }
 
 int main(int argc, char *argv[])
